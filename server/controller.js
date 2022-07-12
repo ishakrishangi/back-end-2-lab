425a19b8394a5c69b3f7afbca4affc2510e68b35
ishakrishangi <krishangik@gmail.com>
2022-07-12 18:56:09 -0400

    fixed adding bug in create house

--- server/controller.js
+++ server/controller.js
@@ -24,7 +24,7 @@ module.exports = {
         let newHouse = {
             id: reallyWhatever,
             address,
-            price,
+            price:+price,
             imageURL
         };
         houses.push(newHouse)
@@ -33,21 +33,21 @@ module.exports = {
     },
 
     updateHouse: (req,res) => {
-        let{id} =req.params
+        let{id} = req.params
         let{type} = req.body
         let index = houses.findIndex(house => house.id === +id)
         if(index === -1){
             res.status(400).send(`user not found`)
         } else if(houses[index].price === 0 && type === 'minus'){
             res.status(400).send(`cannot go below 0`)
+        }  else if(type === 'minus'){
+            houses[index].price -= 10000
+            res.status(200).send(houses)
         } else if(type === 'plus'){
             houses[index].price += 10000
             res.status(200).send(houses)
-        } else if(type === 'minus'){
-            houses[index].price -= 10000
-            res.status(200).send(houses)
         } else{
-            res.sendStatus
+            res.sendStatus(400)
         }
         }
 
